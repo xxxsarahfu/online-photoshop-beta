8f74fd10f59b80de627bc89ee03a52acc981be9a
xxxsarahfu <sarahliu@Sarahde-MacBook-Pro.local>
2018-06-06 22:37:04 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+const base64Img = require("base64-img");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.post("/img", (req, res) => {
+  console.log(req.body.url);
+  var tmp_url = req.body.url;
+  base64Img.img(tmp_url, "dest", "1", function(err, path) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(path);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/App.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import ImageFilter from "react-image-filter";
+// var base64Img = require("base64-img");
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { file: "", imagePreviewUrl: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.refff = "";
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  v;
+
+  handleSubmit(event, ftStr) {
+    alert("Your favorite flavor is: " + this.state.value);
+
+    event.preventDefault();
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log("handle uploading", this.state.file);
+    const fd = new FormData();
+    fd.append("image", this.state.file, this.state.file.name);
+    console.log(fd);
+    axios
+      .post("my-domain.com.file-upload", fd, {
+        onUploadProgress: progressEvent => {
+          console.log(
+            "Upload Progress:" +
+              (progressEvent.loaded / progressEvent.total) * 100 +
+              "%"
+          );
+        }
+      })
+      .then(res => {
+        console.log(res);
+      });
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }
+  _handlePng(e) {
+    e.preventDefault();
+    console.log(this.refff);
+    // axios
+    //   .post("/img", {
+    //     url: url
+    //   })
+    //   .then(res => {
+    //     console.log(res);
+    //   });
+    // base64Img.img(url, "dest", "1", function(err, path) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log(path);
+    //   }
+    // });
+  }
+
+  render() {
+    let { imagePreviewUrl } = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = <img src={imagePreviewUrl} />;
+    } else {
+      $imagePreview = (
+        <div className="previewText">Please select an Image for Preview</div>
+      );
+    }
+    return (
+      <div className="previewComponent">
+        <h1 className="h11"> Online Image Filter</h1>
+        <form onSubmit={e => this._handleSubmit(e)}>
+          <input
+            className="fileInput"
+            type="file"
+            onChange={e => this._handleImageChange(e)}
+          />
+        </form>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Pick your favorite filter:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="invert">invert</option>
+              <option value="grayscale">grayscale</option>
+              <option value="duotone">duotone</option>
+              <option value="sepia">sepia</option>
+            </select>
+          </label>
+        </form>
+        <button
+          className="pngButton"
+          type="submit"
+          onClick={e => this._handlePng(e)}
+        >
+          upload your image to server
+        </button>
+
+        <ImageFilter
+          className="imgPreview"
+          image={imagePreviewUrl}
+          filter={this.state.value} // see docs beneath
+          colorOne={[100, 150, 250]}
+          colorTwo={[250, 150, 130]}
+          ref={tmp => {
+            this.refff = tmp;
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+//ReactDOM.render(<ImageUpload />, document.getElementById("mainApp"));
+
+export default ImageUpload;
